37037a07ca7e67cf8f98345e322b346dd493b25e
hemanitekwani <115246779+hemanitekwani@users.noreply.github.com>
2023-07-20 10:24:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import cv2
+import pixellib
+from pixellib.instance import instance_segmentation
+segment_image = instance_segmentation()
+segment_image.load_model(r"C:\Users\hhema\Downloads\video segmentation\mask_rcnn_coco.h5")
+
+
+
+camera = cv2.VideoCapture(0)
+
+while camera.isOpened():
+    res,frame=camera.read()
+    
+    result=segment_image.segmentFrame(frame,show_bboxes=True)
+    image=result[1]
+    cv2.imshow('Image Segmentation',image)
+
+    if cv2.waitKey(10) & 0xFF==ord('q'):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
